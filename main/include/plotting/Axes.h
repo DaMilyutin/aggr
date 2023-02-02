2175caf82538c8155a260eb5163093b5d9cda3a4
Danila Milyutin <dmilyutin@topcon.com>
2023-02-02 22:43:38 +0300

    Axes

--- main/include/plotting/Axes.h
+++ main/include/plotting/Axes.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "CoordinateSystem.h"
 #include <agge/color.h>
+#include <agge/primitives/figures.h>
+#include <agge/primitives/stroke.h>
+#include <agge/primitives/stroke_features.h>
+
+#include <agge/rendering/platform.h>
+#include <agge/rendering/renderer.h>
+
+
+#include <plotting/Canvas.h>
+
 
 namespace plotting
 {
@@ -8,6 +18,9 @@ namespace plotting
     {
         port_area_t      position;
         CoordinateSystem coordinates;
+
+        agge::color outer { 40, 40, 40, 128};
+        agge::color inner { 20, 20, 20, 255};
 
         struct TickProperties
         {
@@ -30,5 +43,385 @@ namespace plotting
         };
 
         agge::rect<AxisProperties> properties;
+
+        void alignTickStepsX1(double dx)
+        {
+            auto const& pa = coordinates.port_area;
+            properties.x1.tickSteps = int(ceil(fabs(pa.x2-pa.x1)/dx));
+        }
+
+        void alignTickStepsX2(double dx)
+        {
+            auto const& pa = coordinates.port_area;
+            properties.x2.tickSteps = int(ceil(fabs(pa.x2-pa.x1)/dx));
+        }
+
+        void alignTickStepsY1(double dy)
+        {
+            auto const& pa = coordinates.port_area;
+            properties.y1.tickSteps = int(ceil(fabs(pa.y2-pa.y1)/dy));
+        }
+
+        void alignTickStepsY2(double dy)
+        {
+            auto const& pa = coordinates.port_area;
+            properties.y1.tickSteps = int(ceil(fabs(pa.y2-pa.y1)/dy));
+        }
+
     };
+
+    namespace inner
+    {
+        struct Selector_All
+        {
+            bool is() const { return true; }
+            void next() {}
+        };
+
+        struct Selector_Skipper
+        {
+            int index  = 0;
+            int period = 5;
+            bool is() const { return index%period != 0; }
+            void next() { ++index; }
+        };
+
+        struct LineVert
+        {
+            agge::real_t const y1;
+            agge::real_t const y2;
+
+            agge::line make(agge::real_t x) const { return agge::line(x, y1, x, y2); }
+        };
+
+        struct LineHorz
+        {
+            agge::real_t const x1;
+            agge::real_t const x2;
+
+            agge::line make(agge::real_t y) const { return agge::line(x1, y, x2, y); }
+        };
+
+        template<typename LineMaker, typename Selector = Selector_All>
+        struct Range
+        {
+            Selector  mutable   sel;
+            LineMaker           line;
+
+            agge::real_t const  t1;
+            agge::real_t const  t2;
+
+            agge::real_t const  start;
+            agge::real_t const  step;
+
+            agge::color  const& color;
+            agge::stroke&       line_style;
+        };
+
+        template<typename S, typename Rn, typename Rs, typename Line, typename Sel>
+        plotting::Canvas<S, Rn, Rs>& operator<<(plotting::Canvas<S, Rn, Rs>& c,
+                                                Range<Line, Sel> const& rng)
+        {
+            c.ras.reset();
+            agge::real_t t = rng.start;
+            while(t < rng.t1)
+                t += rng.step, rng.sel.next();
+            for(; t<=rng.t2; t += rng.step, rng.sel.next())
+                if(rng.sel.is())
+                    agge::add_path(c.ras, rng.line.make(t)/rng.line_style);
+            c.ras.sort();
+            c.ren(c.surface, agge::zero(), 0 /*no windowing*/, c.ras /*mask*/,
+                  agge::platform_blender_solid_color(rng.color), agge::winding<>());
+            return c;
+        }
+
+        inline double quantize_step(double step_repr)
+        {
+            int const sign = (step_repr>0) - (step_repr<0);
+            step_repr = floor(log(fabs(step_repr))/log(10.));
+            step_repr = sign*exp(step_repr*log(10.));
+            return step_repr;
+        }
+
+        struct AxisTicksMakerX1
+        {
+            AxisTicksMakerX1(Axes const& axes, agge::stroke& line_style)
+                : axes(axes)
+                , prop(axes.properties.x1)
+                , line_style(line_style)
+            {
+                double const step_repr = quantize_step((axes.coordinates.repr_area.x2 - axes.coordinates.repr_area.x1)/prop.tickSteps);
+                double const X_repr = step_repr*ceil(axes.coordinates.repr_area.x1/step_repr);
+                start = agge::real_t(axes.coordinates.scale.x*(X_repr - axes.coordinates.repr_area.x1) + axes.coordinates.port_area.x1);
+                step  = agge::real_t(step_repr*axes.coordinates.scale.x);
+            }
+
+            Range<LineVert, Selector_All> major() const
+            {
+                line_style.width(prop.tick[0].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {{},LineVert{pa.y1-prop.tick[0].length, pa.y1},
+                        pa.x1, pa.x2, start, step,
+                        prop.tick[0].color, line_style};
+            }
+
+            Range<LineVert, Selector_All> medium() const
+            {
+                line_style.width(prop.tick[1].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {{}, LineVert{pa.y1-prop.tick[1].length, pa.y1},
+                        pa.x1, pa.x2, start-step*0.5f, step,
+                        prop.tick[1].color, line_style};
+            }
+
+            Range<LineVert, Selector_Skipper> minor() const
+            {
+                line_style.width(prop.tick[2].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {Selector_Skipper{1, 5},
+                        LineVert{pa.y1-prop.tick[2].length, pa.y1},
+                        pa.x1, pa.x2, start - 0.9f*step, step*0.1f,
+                        prop.tick[2].color, line_style};
+            }
+
+            Axes                 const& axes;
+            Axes::AxisProperties const& prop;
+            agge::stroke& line_style;
+            agge::real_t x1;
+            agge::real_t x2;
+            agge::real_t start;
+            agge::real_t step;
+            agge::real_t y;
+        };
+
+        struct AxisTicksMakerX2
+        {
+            AxisTicksMakerX2(Axes const& axes, agge::stroke& line_style)
+                : axes(axes)
+                , prop(axes.properties.x2)
+                , line_style(line_style)
+            {
+                double const step_repr = quantize_step((axes.coordinates.repr_area.x2 - axes.coordinates.repr_area.x1)/prop.tickSteps);
+                double const X_repr = step_repr*ceil(axes.coordinates.repr_area.x1/step_repr);
+                start = agge::real_t(axes.coordinates.scale.x*(X_repr - axes.coordinates.repr_area.x1) + axes.coordinates.port_area.x1);
+                step = agge::real_t(step_repr*axes.coordinates.scale.x);
+            }
+
+            Range<LineVert, Selector_All> major() const
+            {
+                line_style.width(prop.tick[0].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {{},LineVert{pa.y2, pa.y2 + prop.tick[0].length},
+                       pa.x1, pa.x2, start, step,
+                       prop.tick[0].color, line_style};
+            }
+
+            Range<LineVert, Selector_All> medium() const
+            {
+                line_style.width(prop.tick[1].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {{}, LineVert{pa.y2, pa.y2 + prop.tick[1].length},
+                        pa.x1, pa.x2, start-step*0.5f, step,
+                        prop.tick[1].color, line_style};
+            }
+
+            Range<LineVert, Selector_Skipper> minor() const
+            {
+                line_style.width(prop.tick[2].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {Selector_Skipper{1, 5},
+                        LineVert{pa.y2, pa.y2 + prop.tick[2].length},
+                        pa.x1, pa.x2, start - 0.9f*step, step*0.1f,
+                        prop.tick[2].color, line_style};
+            }
+
+            Axes                 const& axes;
+            Axes::AxisProperties const& prop;
+            agge::stroke& line_style;
+            agge::real_t x1;
+            agge::real_t x2;
+            agge::real_t start;
+            agge::real_t step;
+            agge::real_t y;
+        };
+
+        struct AxisTicksMakerY1
+        {
+            AxisTicksMakerY1(Axes const& axes, agge::stroke& line_style)
+                : axes(axes)
+                , prop(axes.properties.y1)
+                , line_style(line_style)
+            {
+                double const step_repr = quantize_step((axes.coordinates.repr_area.y2 - axes.coordinates.repr_area.y1)/prop.tickSteps);
+                double const Y_repr = step_repr*ceil(axes.coordinates.repr_area.y1/step_repr);
+                start = agge::real_t(axes.coordinates.scale.y*(Y_repr - axes.coordinates.repr_area.y1) + axes.coordinates.port_area.y1);
+                step = agge::real_t(step_repr*axes.coordinates.scale.y);
+            }
+
+            Range<LineHorz, Selector_All> major() const
+            {
+                line_style.width(prop.tick[0].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {{},LineHorz{pa.x1-prop.tick[0].length, pa.x1},
+                       pa.y1, pa.y2, start, step,
+                       prop.tick[0].color, line_style};
+            }
+
+            Range<LineHorz, Selector_All> medium() const
+            {
+                line_style.width(prop.tick[1].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {{}, LineHorz{pa.x1-prop.tick[1].length, pa.x1},
+                        pa.y1, pa.y2, start-step*0.5f, step,
+                        prop.tick[1].color, line_style};
+            }
+
+            Range<LineHorz, Selector_Skipper> minor() const
+            {
+                line_style.width(prop.tick[2].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {Selector_Skipper{1, 5},
+                        LineHorz{pa.x1-prop.tick[2].length, pa.x1},
+                        pa.y1, pa.y2, start - 0.9f*step, step*0.1f,
+                        prop.tick[2].color, line_style};
+            }
+
+            Axes                 const& axes;
+            Axes::AxisProperties const& prop;
+            agge::stroke& line_style;
+            agge::real_t x1;
+            agge::real_t x2;
+            agge::real_t start;
+            agge::real_t step;
+            agge::real_t y;
+        };
+
+        struct AxisTicksMakerY2
+        {
+            AxisTicksMakerY2(Axes const& axes, agge::stroke& line_style)
+                : axes(axes)
+                , prop(axes.properties.y2)
+                , line_style(line_style)
+            {
+                double const step_repr = quantize_step((axes.coordinates.repr_area.y2 - axes.coordinates.repr_area.y1)/prop.tickSteps);
+                double const Y_repr = step_repr*ceil(axes.coordinates.repr_area.y1/step_repr);
+                start = agge::real_t(axes.coordinates.scale.y*(Y_repr - axes.coordinates.repr_area.y1) + axes.coordinates.port_area.y1);
+                step = agge::real_t(step_repr*axes.coordinates.scale.y);
+            }
+
+            Range<LineHorz, Selector_All> major() const
+            {
+                line_style.width(prop.tick[0].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {{},LineHorz{pa.x2, pa.x2+prop.tick[0].length},
+                       pa.y1, pa.y2, start, step,
+                       prop.tick[0].color, line_style};
+            }
+
+            Range<LineHorz, Selector_All> medium() const
+            {
+                line_style.width(prop.tick[1].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {{}, LineHorz{pa.x2, pa.x2+prop.tick[1].length},
+                        pa.y1, pa.y2, start-step*0.5f, step,
+                        prop.tick[1].color, line_style};
+            }
+
+            Range<LineHorz, Selector_Skipper> minor() const
+            {
+                line_style.width(prop.tick[2].width);
+                auto const& pa = axes.coordinates.port_area;
+                return {Selector_Skipper{1, 5},
+                        LineHorz{pa.x2, pa.x2+prop.tick[2].length},
+                        pa.y1, pa.y2, start - 0.9f*step, step*0.1f,
+                        prop.tick[2].color, line_style};
+            }
+
+            Axes                 const& axes;
+            Axes::AxisProperties const& prop;
+            agge::stroke& line_style;
+            agge::real_t x1;
+            agge::real_t x2;
+            agge::real_t start;
+            agge::real_t step;
+            agge::real_t y;
+        };
+
+        template<typename S, typename Rn, typename Rs>
+        plotting::Canvas<S, Rn, Rs>& operator<<(plotting::Canvas<S, Rn, Rs>& c,
+                                                AxisTicksMakerX1 const& ticks)
+        {
+            return c << ticks.major() << ticks.medium() << ticks.minor();
+        }
+
+        template<typename S, typename Rn, typename Rs>
+        plotting::Canvas<S, Rn, Rs>& operator<<(plotting::Canvas<S, Rn, Rs>& c,
+            AxisTicksMakerX2 const& ticks)
+        {
+            return c << ticks.major() << ticks.medium() << ticks.minor();
+        }
+
+        template<typename S, typename Rn, typename Rs>
+        plotting::Canvas<S, Rn, Rs>& operator<<(plotting::Canvas<S, Rn, Rs>& c,
+            AxisTicksMakerY1 const& ticks)
+        {
+            return c << ticks.major() << ticks.medium() << ticks.minor();
+        }
+
+        template<typename S, typename Rn, typename Rs>
+        plotting::Canvas<S, Rn, Rs>& operator<<(plotting::Canvas<S, Rn, Rs>& c,
+            AxisTicksMakerY2 const& ticks)
+        {
+            return c << ticks.major() << ticks.medium() << ticks.minor();
+        }
+    }
+
+
+    template<typename S, typename Rn, typename Rs>
+    Canvas<S, Rn, Rs>& operator<<(Canvas<S, Rn, Rs>& c, Axes const& axes)
+    {
+        c.ras.reset_clipping();
+        auto to_int = [](agge::rect_r const& r) { return agge::rect_i{int(round(r.x1)), int(round(r.y1)),
+                                                                      int(round(r.x2)), int(round(r.y2))}; };
+        agge::fill(c.surface, to_int(axes.position),
+                   agge::platform_blender_solid_color(axes.outer));
+        c.ras.set_clipping(axes.position);
+
+        agge::fill(c.surface, to_int(axes.coordinates.port_area),
+                   agge::platform_blender_solid_color(axes.inner));
+
+        agge::stroke line_style;
+        line_style.set_cap(agge::caps::butt());
+        line_style.set_join(agge::joins::bevel());
+
+        c << reset
+            << agge::line(axes.coordinates.port_area.x1, axes.coordinates.port_area.y1,
+                        axes.coordinates.port_area.x1, axes.coordinates.port_area.y2)
+                        / line_style.width(axes.properties.x1.sep.width)
+            << agge::platform_blender_solid_color(axes.properties.x1.sep.color);
+
+        c << reset
+            << agge::line(axes.coordinates.port_area.x2, axes.coordinates.port_area.y1,
+                            axes.coordinates.port_area.x2, axes.coordinates.port_area.y2)
+                    / line_style.width(axes.properties.x2.sep.width)
+            << agge::platform_blender_solid_color(axes.properties.x2.sep.color);
+
+        c << reset
+            << agge::line(axes.coordinates.port_area.x1, axes.coordinates.port_area.y1,
+                          axes.coordinates.port_area.x2, axes.coordinates.port_area.y1)
+                    / line_style.width(axes.properties.y1.sep.width)
+            << agge::platform_blender_solid_color(axes.properties.y1.sep.color);
+
+        c << reset
+            << agge::line(axes.coordinates.port_area.x1, axes.coordinates.port_area.y2,
+                          axes.coordinates.port_area.x2, axes.coordinates.port_area.y2)
+                    / line_style.width(axes.properties.y2.sep.width)
+            << agge::platform_blender_solid_color(axes.properties.y2.sep.color);
+
+        c << inner::AxisTicksMakerX1{axes, line_style}
+          << inner::AxisTicksMakerX2{axes, line_style}
+          << inner::AxisTicksMakerY1{axes, line_style}
+          << inner::AxisTicksMakerY2{axes, line_style};
+        return c;
+    }
 }
--- test/samples/plotting/plotting.cpp
+++ test/samples/plotting/plotting.cpp
@@ -39,172 +39,14 @@ namespace
         }
     private:
 
-        void drawAxesArea(platform_bitmap& surface)
-        {
-            ras.reset();
-            ras.set_clipping(axes.position);
-            //axes.properties.x1;
-            line_style.width(axes.properties.x1.sep.width);
-            line_style.set_cap(caps::butt());
-            line_style.set_join(joins::bevel());
-            add_path(ras,
-                      agge::line(axes.coordinates.port_area.x1, axes.coordinates.port_area.y1,
-                                 axes.coordinates.port_area.x1, axes.coordinates.port_area.y2)
-                                 / line_style);
-            ras.sort();
-
-            ren(surface, zero(), 0 /*no windowing*/, ras /*mask*/,
-                platform_blender_solid_color(axes.properties.x1.sep.color), winding<>());
-            ras.reset();
-
-            line_style.width(axes.properties.x2.sep.width);
-            line_style.set_cap(caps::butt());
-            line_style.set_join(joins::bevel());
-            add_path(ras,
-                agge::line(axes.coordinates.port_area.x2, axes.coordinates.port_area.y1,
-                           axes.coordinates.port_area.x2, axes.coordinates.port_area.y2)
-                           / line_style);
-            ras.sort();
-
-            ren(surface, zero(), 0 /*no windowing*/, ras /*mask*/,
-                platform_blender_solid_color(axes.properties.x2.sep.color), winding<>());
-            ras.reset();
-
-            {
-                double step_repr = (axes.coordinates.repr_area.x2 - axes.coordinates.repr_area.x1)/axes.properties.x1.tickSteps;
-                step_repr = floor(log(step_repr)/log(10.));
-                step_repr = exp(step_repr*log(10.));
-                double const X_repr = step_repr*ceil(axes.coordinates.repr_area.x1/step_repr);
-                real_t const x1 = real_t(axes.coordinates.scale.x*(X_repr-axes.coordinates.repr_area.x1) + axes.coordinates.port_area.x1);
-                real_t const step = real_t(step_repr*axes.coordinates.scale.x);
-
-                real_t const x2 = axes.coordinates.port_area.x2;
-
-                auto major = [&](real_t const y1, real_t const y2,
-                                                        plotting::Axes::TickProperties const& tick)
-                {
-                    ras.reset();
-                    line_style.width(tick.width);
-                    line_style.set_cap(caps::butt());
-                    line_style.set_join(joins::bevel());
-                    for(real_t x = x1; x<=x2; x += step)
-                        add_path(ras, agge::line(x, y1, x, y2)/line_style);
-
-                    ras.sort();
-                    ren(surface, zero(), 0 /*no windowing*/, ras /*mask*/,
-                        platform_blender_solid_color(tick.color), winding<>());
-                };
-
-                auto middle = [&](real_t const y1, real_t const y2,
-                    plotting::Axes::TickProperties const& tick)
-                {
-                    ras.reset();
-                    line_style.width(tick.width);
-                    line_style.set_cap(caps::butt());
-                    line_style.set_join(joins::bevel());
-                    real_t x = x1 - step*0.5f;
-                    while(x < axes.coordinates.port_area.x1)
-                        x += step;
-                    for(; x<=x2; x += step)
-                        add_path(ras, agge::line(x, y1, x, y2)/line_style);
-
-                    ras.sort();
-                    ren(surface, zero(), 0 /*no windowing*/, ras /*mask*/,
-                        platform_blender_solid_color(tick.color), winding<>());
-                };
-
-                auto minor = [&](real_t const y1, real_t const y2, real_t step,
-                    plotting::Axes::TickProperties const& tick)
-                {
-                    step *= 0.1f;
-                    ras.reset();
-                    line_style.width(tick.width);
-                    line_style.set_cap(caps::butt());
-                    line_style.set_join(joins::bevel());
-                    int i = 1;
-                    real_t x = x1 - step*9.0f;
-                    while(x < axes.coordinates.port_area.x1)
-                        x += step, ++i;
-                    for(; x<=x2; x += step)
-                    {
-                        if(i++ % 5 == 0)
-                            continue;
-                        add_path(ras, agge::line(x, y1, x, y2)/line_style);
-                    }
-
-                    ras.sort();
-                    ren(surface, zero(), 0 /*no windowing*/, ras /*mask*/,
-                        platform_blender_solid_color(tick.color), winding<>());
-                };
-
-
-                {
-                    real_t const y2 = axes.coordinates.port_area.y1;
-                    real_t const y1 = axes.coordinates.port_area.y1 - axes.properties.y1.tick[0].length;
-                    major(y1, y2, axes.properties.y1.tick[0]);
-                }
-                {
-                    real_t const y1 = axes.coordinates.port_area.y2;
-                    real_t const y2 = axes.coordinates.port_area.y2 + axes.properties.y2.tick[0].length;
-                    major(y1, y2, axes.properties.y2.tick[0]);
-                }
-                {
-                    real_t const y2 = axes.coordinates.port_area.y1;
-                    real_t const y1 = axes.coordinates.port_area.y1 - axes.properties.y1.tick[1].length;
-                    middle(y1, y2, axes.properties.y1.tick[1]);
-                }
-                {
-                    real_t const y1 = axes.coordinates.port_area.y2;
-                    real_t const y2 = axes.coordinates.port_area.y2 + axes.properties.y2.tick[1].length;
-                    middle(y1, y2, axes.properties.y2.tick[1]);
-                }
-                {
-                    real_t const y2 = axes.coordinates.port_area.y1;
-                    real_t const y1 = axes.coordinates.port_area.y1 - axes.properties.y1.tick[2].length;
-                    minor(y1, y2, step, axes.properties.y1.tick[2]);
-                }
-                {
-                    real_t const y1 = axes.coordinates.port_area.y2;
-                    real_t const y2 = axes.coordinates.port_area.y2 + axes.properties.y2.tick[2].length;
-                    minor(y1, y2, step, axes.properties.y2.tick[2]);
-                }
-
-            }
-
-            line_style.width(axes.properties.y1.sep.width);
-            line_style.set_cap(caps::butt());
-            line_style.set_join(joins::bevel());
-            auto canvas = plotting::make_canvas(surface, ren, ras);
-
-            canvas << plotting::reset
-                   << agge::line(axes.coordinates.port_area.x1, axes.coordinates.port_area.y1,
-                                  axes.coordinates.port_area.x2, axes.coordinates.port_area.y1)
-                                 / line_style
-                   << platform_blender_solid_color(axes.properties.y1.sep.color);
-
-            ras.reset();
-            line_style.width(axes.properties.y2.sep.width);
-            line_style.set_cap(caps::butt());
-            line_style.set_join(joins::bevel());
-            add_path(ras,
-                     agge::line(axes.coordinates.port_area.x1, axes.coordinates.port_area.y2,
-                                axes.coordinates.port_area.x2, axes.coordinates.port_area.y2)
-                        /line_style);
-            ras.sort();
-
-            ren(surface, zero(), 0 /*no windowing*/, ras /*mask*/,
-                platform_blender_solid_color(axes.properties.y2.sep.color), winding<>());
-            ras.reset();
-        }
-
-        virtual void draw(platform_bitmap& surface, timings&/*timings*/)
+        virtual void draw(agge::platform_bitmap& surface, timings&/*timings*/)
         {
             fill(surface, mkrect<int>(0, 0, surface.width(), surface.height()),
-                platform_blender_solid_color(color::make(10, 10, 10)));
+                 agge::platform_blender_solid_color(color::make(10, 10, 10)));
 
-            drawAxesArea(surface);
+            auto canvas = plotting::make_canvas(surface, ren, ras);
 
-            ras.set_clipping(axes.coordinates.port_area);
+            canvas << axes;
 
             auto wid = float(surface.width());
             auto hei = float(surface.height());
@@ -216,8 +58,6 @@ namespace
             dash_style.remove_all_dashes();
             dash_style.add_dash(10.0f*scale, 5.0f*scale);
             dash_style.dash_start(0.5f*scale);
-
-            auto canvas = plotting::make_canvas(surface, ren, ras);
 
             canvas << plotting::reset
                 << agge::polyline_adaptor(points1)/dash_style/line_style
@@ -233,6 +73,11 @@ namespace
             auto const F = [](double t) { return cos(t)*exp(0.5*t); };
             axes.position = plotting::port_area_t{10.0f, 10.0f, (float)width-10.0f, (float)height-10.0f};
             axes.coordinates.update(plotting::port_area_t{100.0f, 40.0f, (float)width-20.0f, (float)height-100.0f});
+
+            axes.alignTickStepsX1(150);
+            axes.alignTickStepsX2(300);
+            axes.alignTickStepsY1(150);
+            axes.alignTickStepsY2(300);
 
             {
                 double t = -5.;
