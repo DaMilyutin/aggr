0448d4575c5f6a0684cc6c7931d23346f1c671a8
Danila Milyutin <dmilyutin@topcon.com>
2023-02-06 01:49:31 +0300

    optimized calculation of coordinates

--- main/include/plotting/CoordinateSystem.h
+++ main/include/plotting/CoordinateSystem.h
@@ -22,31 +22,34 @@ namespace plotting
             port_area = p;
             scale.x = (double(port_area.x2)-port_area.x1)/(repr_area.x2-repr_area.x1);
             scale.y = (double(port_area.y2)-port_area.y1)/(repr_area.y2-repr_area.y1);
+            offset.x = port_area.x1 - scale.x*repr_area.x1;
+            offset.y = port_area.y1 - scale.y*repr_area.y1;
         }
 
         repr_area_t repr_area = repr_area_t{0., 1., 0., 1.};
         port_area_t port_area = port_area_t{agge::real_t(repr_area.x1), agge::real_t(repr_area.y1)
                                            ,agge::real_t(repr_area.x2), agge::real_t(repr_area.y2)};
-        repr_t      scale = repr_t{1., 1.};
+        repr_t      scale  = repr_t{1., 1.};
+        repr_t      offset = repr_t{0., 0.};
 
         agge::real_t to_port_x(double x) const
         {
-            return agge::real_t(port_area.x1 + scale.x*(x - repr_area.x1));
+            return agge::real_t(offset.x + scale.x*x);
         }
 
         agge::real_t to_port_y(double y) const
         {
-            return agge::real_t(port_area.y1 + scale.y*(y - repr_area.y1));
+            return agge::real_t(offset.y + scale.y*y);
         }
 
         double to_repr_x(agge::real_t x) const
         {
-            return repr_area.x1 + double(x - port_area.x1)/scale.x;
+            return double(x - offset.x)/scale.x;
         }
 
         double to_repr_y(agge::real_t y) const
         {
-            return repr_area.y1 + double(y - port_area.y1)/scale.y;
+            return double(y - offset.y)/scale.y;
         }
 
     };
