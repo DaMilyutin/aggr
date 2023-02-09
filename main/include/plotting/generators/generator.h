e1458183ca6e6536daffc6725a62548c3ddf5d73
Danila Milyutin <dmilyutin@topcon.com>
2023-02-09 08:38:59 +0300

    decouple with agge::pipeline::terminal

--- main/include/plotting/generators/generator.h
+++ main/include/plotting/generators/generator.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <agge/primitives/pipeline.h>
 #include <utility>
 #define FWD(a) std::forward<decltype(a)>(a)
 
@@ -8,15 +7,23 @@ namespace plotting
     namespace pipeline
     {
         template<typename E>
-        struct Generator: public agge::pipeline::terminal<E>
+        struct _terminal_
+        {
+            E& _get_()& { return static_cast<E&>(*this); }
+            E const& _get_() const& { return static_cast<E const&>(*this); }
+            E&& _get_()&& { return static_cast<E&&>(*this); }
+        };
+
+        template<typename E>
+        struct Generator: public _terminal_<E>
         {};
 
         template<typename E>
-        struct Chain: public agge::pipeline::terminal<E>
+        struct Chain: public _terminal_<E>
         {};
 
         template<typename E>
-        struct Sink: public agge::pipeline::terminal<E>
+        struct Sink: public _terminal_<E>
         {};
     }
 }
