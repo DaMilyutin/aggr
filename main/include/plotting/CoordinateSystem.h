a63fcd4aa81a48e846f182683fa25b0cb62c05d1
Danila Milyutin <dmilyutin@topcon.com>
2023-02-09 09:15:09 +0300

    reorganize helpers

--- main/include/plotting/CoordinateSystem.h
+++ main/include/plotting/CoordinateSystem.h
@@ -1,72 +1,10 @@
 #pragma once
 
 #include <plotting/types/point_types.h>
-#include <plotting/generators/transform.h>
+#include <plotting/chain-helpers/transformers.h>
 
 namespace plotting
 {
-    struct ReprToPort: pipeline::Transform<ReprToPort>
-    {
-        void from(repr_area_t const& repr_area, port_area_t const& port_area)
-        {
-            scale.x = (double(port_area.x2)-port_area.x1)/(repr_area.x2-repr_area.x1);
-            scale.y = (double(port_area.y2)-port_area.y1)/(repr_area.y2-repr_area.y1);
-            offset.x = port_area.x1 - scale.x*repr_area.x1;
-            offset.y = port_area.y1 - scale.y*repr_area.y1;
-        }
-
-        port_t operator()(repr_t const& t) const
-        {
-            return {agge::real_t(offset.x + scale.x*t.x), agge::real_t(offset.y + scale.y*t.y)};
-        }
-
-        agge::real_t x(double v) const
-        {
-            return agge::real_t(offset.x + scale.x*v);
-        }
-
-        agge::real_t y(double v) const
-        {
-            return agge::real_t(offset.y + scale.y*v);
-        }
-
-        repr_t      scale  = repr_t{1., 1.};
-        repr_t      offset = repr_t{0., 0.};
-    };
-
-    struct PortToRepr: pipeline::Transform<PortToRepr>
-    {
-        void from(port_area_t const& port_area, repr_area_t const& repr_area)
-        {
-            scale.x = (repr_area.x2-repr_area.x1)/(double(port_area.x2)-port_area.x1);
-            scale.y = (repr_area.y2-repr_area.y1)/(double(port_area.y2)-port_area.y1);
-            offset.x = repr_area.x1 - scale.x*port_area.x1;
-            offset.y = repr_area.y1 - scale.y*port_area.y1;
-        }
-
-        repr_t operator()(port_t const& t) const
-        {
-            return {(offset.x + scale.x*t.x), (offset.y + scale.y*t.y)};
-        }
-        repr_diff_t operator()(port_diff_t const& t) const
-        {
-            return {(scale.x*t.dx), (scale.y*t.dy)};
-        }
-
-        double x(agge::real_t v) const
-        {
-            return offset.x + scale.x*v;
-        }
-
-        double y(agge::real_t v) const
-        {
-            return offset.y + scale.y*v;
-        }
-
-        repr_t      scale = repr_t{1., 1.};
-        repr_t      offset = repr_t{0., 0.};
-    };
-
     struct CoordinateSystem
     {
         CoordinateSystem() = default;
@@ -92,13 +30,13 @@ namespace plotting
         port_area_t port_area = port_area_t{agge::real_t(repr_area.x1), agge::real_t(repr_area.y1)
                                            ,agge::real_t(repr_area.x2), agge::real_t(repr_area.y2)};
 
-        ReprToPort repr2port;
-        PortToRepr port2repr;
+        transformers::ReprToPort repr2port;
+        transformers::PortToRepr port2repr;
     };
 
-    inline port_t operator/(repr_t const& f, ReprToPort const& c)
+    inline auto clip_rect2port(CoordinateSystem const& c)
     {
-        return c(f);
+        return transformers::ReprAreaToViewPortClipper(c.repr_area, c.repr2port);
     }
 
     inline void shift(repr_area_t& a, repr_diff_t const& v)
---
+++ main/include/plotting/chain-helpers/transformers.h
@@ -0,0 +1,101 @@
+#pragma once
+#include <plotting/generators/transform.h>
+#include <plotting/generators/transform_or.h>
+
+#include <plotting/types/point_types.h>
+
+#include <agge/primitives/polyline.h>
+#include <optional>
+
+namespace plotting
+{
+    namespace transformers
+    {
+        struct ReprToPort: pipeline::Transform<ReprToPort>
+        {
+            void from(repr_area_t const& repr_area, port_area_t const& port_area)
+            {
+                scale.x = (double(port_area.x2)-port_area.x1)/(repr_area.x2-repr_area.x1);
+                scale.y = (double(port_area.y2)-port_area.y1)/(repr_area.y2-repr_area.y1);
+                offset.x = port_area.x1 - scale.x*repr_area.x1;
+                offset.y = port_area.y1 - scale.y*repr_area.y1;
+            }
+
+            port_t operator()(repr_t const& t) const
+            {
+                return {agge::real_t(offset.x + scale.x*t.x), agge::real_t(offset.y + scale.y*t.y)};
+            }
+
+            agge::real_t x(double v) const
+            {
+                return agge::real_t(offset.x + scale.x*v);
+            }
+
+            agge::real_t y(double v) const
+            {
+                return agge::real_t(offset.y + scale.y*v);
+            }
+
+            repr_t      scale = repr_t{1., 1.};
+            repr_t      offset = repr_t{0., 0.};
+        };
+
+        struct PortToRepr: pipeline::Transform<PortToRepr>
+        {
+            void from(port_area_t const& port_area, repr_area_t const& repr_area)
+            {
+                scale.x = (repr_area.x2-repr_area.x1)/(double(port_area.x2)-port_area.x1);
+                scale.y = (repr_area.y2-repr_area.y1)/(double(port_area.y2)-port_area.y1);
+                offset.x = repr_area.x1 - scale.x*port_area.x1;
+                offset.y = repr_area.y1 - scale.y*port_area.y1;
+            }
+
+            repr_t operator()(port_t const& t) const
+            {
+                return {(offset.x + scale.x*t.x), (offset.y + scale.y*t.y)};
+            }
+            repr_diff_t operator()(port_diff_t const& t) const
+            {
+                return {(scale.x*t.dx), (scale.y*t.dy)};
+            }
+
+            double x(agge::real_t v) const
+            {
+                return offset.x + scale.x*v;
+            }
+
+            double y(agge::real_t v) const
+            {
+                return offset.y + scale.y*v;
+            }
+
+            repr_t      scale = repr_t{1., 1.};
+            repr_t      offset = repr_t{0., 0.};
+        };
+
+        class ReprAreaToViewPortClipper: public pipeline::TransformOr<ReprAreaToViewPortClipper>
+        {
+            unsigned mutable cmd = agge::path_command_line_to;
+            unsigned mutable next = agge::path_command_line_to;
+            plotting::repr_area_t const& area;
+            ReprToPort  const& transform;
+        public:
+            ReprAreaToViewPortClipper(plotting::repr_area_t const& area,
+                                      ReprToPort const& transform)
+                : area(area), transform(transform)
+            {}
+
+            std::optional<agge::polyline::Item> operator()(plotting::repr_t const& r) const
+            {
+                if(!in_area(area, r))
+                {
+                    cmd = next = agge::path_command_move_to;
+                    return std::optional<agge::polyline::Item>();
+                }
+                cmd = next;
+                next = agge::path_command_line_to;
+                return agge::polyline::Item{transform(r), cmd};
+            }
+        };
+    }
+}
