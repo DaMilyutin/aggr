0aeef41a50f0a53107eaf911a35c7387440a317a
Danila Milyutin <dmilyutin@topcon.com>
2023-02-16 08:24:30 +0300

    cleanup

--- main/include/plotting/piping/elements.h
+++ main/include/plotting/piping/elements.h
@@ -16,28 +16,29 @@ namespace plotting
         template<typename T>             auto range(T b, T e, T step) { return ylems::elements::range<terminal>(b, e, step); }
         template< typename T>            auto range(T b, T e) { return ylems::elements::range<terminal>(b, e); }
 
-        template<typename F> auto filter(F&& f) { return ylems::elements::filter<terminal>(FWD(f)); }
-        template<typename F> auto transform(F&& f) { return ylems::elements::transform<terminal>(FWD(f)); }
-        template<typename F> auto transform_or(F&& f) { return ylems::elements::transform_or<terminal>(FWD(f)); }
-        template<typename F, typename G> auto transform_or(F&& f, G&& g) { return ylems::elements::transform_or<terminal>(FWD(f), FWD(g)); }
+        template<typename Y> auto yield(Y&& y) { return ylems::elements::yield<terminal>(FWD(y)); }
         template<typename... T> auto zip(T&&... f) { return ylems::elements::zip<terminal>(FWD(f)...); }
 
+        template<typename F> auto filter(F&& f) { return ylems::elements::filter<terminal>(FWD(f)); }
         template<typename I> auto take(I i) { return ylems::elements::take<terminal>(i); }
         template<typename I> auto drop(I i) { return ylems::elements::drop<terminal>(i); }
 
-        template<typename Y> auto yield(Y&& y) { return ylems::elements::yield<terminal>(FWD(y)); }
+        template<typename F> auto transform(F&& f) { return ylems::elements::transform<terminal>(FWD(f)); }
+        template<typename F> auto transform_or(F&& f) { return ylems::elements::transform_or<terminal>(FWD(f)); }
+        template<typename F, typename G> auto transform_or(F&& f, G&& g) { return ylems::elements::transform_or<terminal>(FWD(f), FWD(g)); }
     }
 
     using piping::iota;
     using piping::linspace;
     using piping::range;
 
-    using piping::filter;
-    using piping::transform;
-    using piping::transform;
-
+    using piping::yield;
     using piping::zip;
 
+    using piping::filter;
     using piping::take;
     using piping::drop;
+
+    using piping::transform;
+    using piping::transform_or;
 }
