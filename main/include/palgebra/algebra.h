b09f69b7381f6d5ef079496d59b80b4d29c9015f
Danila Milyutin <dmilyutin@topcon.com>
2023-03-05 20:47:14 +0300

    remove "distributive law". not working

--- main/include/palgebra/algebra.h
+++ main/include/palgebra/algebra.h
@@ -7,32 +7,3 @@
 #include "algebra/closed.h"
 #include "algebra/decorated.h"
 
-namespace agge
-{
-    namespace rules
-    {
-        template<typename P1, typename P2, typename D>
-        auto operator/(Joined<P1, P2>&& j, Decorator<D>&& d)
-        {
-            return FWD(FWD(j).p1)/FWD(d)._get_() + FWD(FWD(j).p2)/FWD(d)._get_();
-        }
-
-        template<typename P1, typename P2, typename D>
-        auto operator/(Joined<P1, P2> const& j, Decorator<D>&& d)
-        {
-            return j.p1/FWD(d)._get_() + j.p2/FWD(d)._get_();
-        }
-
-        template<typename P1, typename P2, typename D>
-        auto operator/(Joined<P1, P2>&& j, Decorator<D> const& d)
-        {
-            return FWD(FWD(j).p1)/d._get_() + FWD(FWD(j).p2)/d._get_();
-        }
-
-        template<typename P1, typename P2, typename D>
-        auto operator/(Joined<P1, P2> const& j, Decorator<D> const& d)
-        {
-            return j.p1/d._get_() + j.p2/d._get_();
-        }
-    }
-}
