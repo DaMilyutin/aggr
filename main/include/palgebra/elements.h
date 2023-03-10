013789fc16f484e979a688847341ca3f9e7d72e9
Danila Milyutin <dmilyutin@topcon.com>
2023-03-10 03:43:11 +0300

    include fix bezier

--- main/include/palgebra/elements.h
+++ main/include/palgebra/elements.h
@@ -1,4 +1,4 @@
 #include <palgebra/elements/vertex.h>
 #include <palgebra/elements/segment.h>
 #include <palgebra/elements/arc.h>
-#include <palgebra/elements/curve.h>
+#include <palgebra/elements/bezier.h>
